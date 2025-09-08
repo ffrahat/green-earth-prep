be524efe5ba5dee233c94e2f3e3aa86c02e4fcf8
Mahialam Rahat <mahialam408@gmail.com>
2025-09-08 20:45:58 +0600

    bal

--- script.js
+++ script.js
@@ -11,65 +11,57 @@ navIcon.addEventListener("click", () => {
 
 // Get The api
 
-const url = "https://openapi.programming-hero.com/api/plants";
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => pushData(data));
-
-const pushData = (catchObj) => {
-  // console.log(catchObj)
-
-  const plants = catchObj.plants;
-
-  const fontPageShows = plants.slice(0, 9);
-    fontPageShows.forEach((fontPage) => {
-      // console.log(fontPage)
-      const parentCart = document.getElementById('parent-cart')
-      const cart = document.createElement('div')
-      cart.innerHTML = `
-      
-                <div class="cart p-5 mb-5 bg-[#fff] rounded-lg">
-                        <div class="img">
-                            <img class="mx-auto w-full mb-3 h-[200px] object-cover rounded-lg" src="${fontPage.image}" alt="">
-                        </div> 
-                        <h1 class="mb-3 font-semibold text-2xl">${fontPage.name}</h1>
-                        <p class="mb-3">${fontPage.description}</p>
-                        
-                        <div class="category-price flex items-center justify-between mb-4">
-                            <div class="category">
-                                <p class="text-lg px-2 rounded-xl text-[#15803d] bg-[#dcfce7]">${fontPage.category}</p>
-                            </div>
-
-                            <div class="price">
-                                <p class="font-bold text-lg  text-[#15803d]"><span>${fontPage.price}</span> $</p>
-                            </div>
-                            
-                        </div>
-
-                        <button type="submit" class="btn btn-active btn-success rounded-[20px] bg-[#15803d] w-full p-5 border-none outline-none text-[#fff] add-to-cart">Add to Cart</button>
+const loadAllPlants = () => {
+  const url = 'https://openapi.programming-hero.com/api/plants'
+  fetch(url)
+    .then(res => res.json())
+    .then(data => pushAllPlants(data))
+}
+const pushAllPlants = (allplantsdata) => {
+  // console.log(allplantsdata.plants)
+}
 
 
-                   </div>
-      
-      
-      `
 
-      parentCart.appendChild(cart)
 
-      // new 
+// Category Loads 
 
-      const myCarts = cart.querySelectorAll('.add-to-cart')
-      myCarts.forEach(mycart => {
-        mycart.addEventListener('click', (e) => {
-          console.log(mycart.parentNode)
-        })
-      })
-  });
-    
-    
+const loadCategories = () => {
+  const url = 'https://openapi.programming-hero.com/api/categories'
+  fetch(url)
+    .then(res => res.json())
+    .then(data => allCategories(data))
+}
 
-  plants.forEach((plant) => {
-    // console.log(plant)
-  });
-};
+
+const allCategories = (categoriesData) => {
+  const mainCategories = categoriesData.categories
+  const categoriesParent = document.getElementById('categories-parent')
+  mainCategories.forEach(category => {
+    const categoriesBtn = document.createElement('button')
+    console.log(category)
+    categoriesBtn.className = "md:w-full btn-outline border md:text-lg md:border-none md:outline-none md:bg-transparent hover:bg-[#15803d] hover:text-[#fff] text-left p-2 mr-2 mb-2 text-[#000000] text-left ct-btn-hover"
+    categoriesBtn.textContent = `${category.category_name }`
+
+    categoriesParent.appendChild(categoriesBtn)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+loadCategories()
+loadAllPlants();
