24e50909cd94169c471aaba4cf427c115ae4238b
Mahialam Rahat <mahialam408@gmail.com>
2025-09-09 00:20:54 +0600

    obosese

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ navIcon.addEventListener("click", () => {
   navBar.classList.toggle("nav-shows");
 });
 
+
+
 // Get The api
 
 
@@ -19,7 +21,7 @@ const loadAllPlants = () => {
     .then(data => pushAllPlants(data))
 }
 const pushAllPlants = (allplantsdata) => {
-  // console.log(allplantsdata.plants)
+  console.log(allplantsdata.plants)
 }
 
 
@@ -28,7 +30,7 @@ const pushAllPlants = (allplantsdata) => {
 // Category Loads 
 
 const loadCategories = () => {
-  const url = 'https://openapi.programming-hero.com/api/categories'
+  const url = 'https://openapi.programming-hero.com/api/categories' 
   fetch(url)
     .then(res => res.json())
     .then(data => allCategories(data))
@@ -42,10 +44,63 @@ const allCategories = (categoriesData) => {
     const categoriesBtn = document.createElement('button')
     console.log(category)
     categoriesBtn.className = "md:w-full btn-outline border md:text-lg md:border-none md:outline-none md:bg-transparent hover:bg-[#15803d] hover:text-[#fff] text-left p-2 mr-2 mb-2 text-[#000000] text-left ct-btn-hover"
-    categoriesBtn.textContent = `${category.category_name }`
-
+    categoriesBtn.textContent = `${category.category_name}`
+    categoriesBtn.onclick = () => categoryBtnClick(category.id)
+    
     categoriesParent.appendChild(categoriesBtn)
   })
+}
+
+
+const categoryBtnClick = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => childCreatedByCategories(data))
+}
+
+
+const childCreatedByCategories = (ctId) => {
+  const plants = ctId.plants
+  console.log(plants)
+    const parentCart = document.getElementById('parent-cart')
+    parentCart.innerHTML = ""
+  plants.forEach(plant => {
+    console.log(plant)
+    const childCart = document.createElement('div');
+    childCart.innerHTML = `
+    
+    <!-- cart made by rahat -->
+
+                    <div class="cart p-5 mb-5 bg-[#fff] rounded-lg">
+                        <div class="img">
+                            <img class="mx-auto w-full mb-3 h-[200px] object-cover rounded-lg" src="${plant.image}" alt="">
+                        </div> 
+                        <h1 class="mb-3 font-semibold text-2xl">${plant.name}</h1>
+                        <p class="mb-3">${plant.description}</p>
+                        
+                        <div class="category-price flex items-center justify-between mb-4">
+                            <div class="category">
+                                <p class="text-lg px-2 rounded-xl text-[#15803d] bg-[#dcfce7]">${plant.category}</p>
+                            </div>
+
+                            <div class="price">
+                                <p class="font-bold text-lg  text-[#15803d]"><span>${plant.price}</span> $</p>
+                            </div>
+                            
+                        </div>
+
+                        <button type="submit" class="btn btn-active btn-success rounded-[20px] bg-[#15803d] w-full p-5 border-none outline-none text-[#fff] add-to-cart">Add to Cart</button>
+
+
+                   </div>
+      
+    
+    `
+     parentCart.appendChild(childCart);
+  })
+
+ 
 }
 
 
